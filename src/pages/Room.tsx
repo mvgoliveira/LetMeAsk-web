5b5d221c663f38ba3ab29990123b542ac0f89d50
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2021-07-03 14:26:21 -0300

    ✨ Added room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,34 @@
+import { Container } from '../styles/room';
+
+import logoImg from '../assets/images/logo.svg'
+import { Button } from '../components/Button';
+
 export function Room() {
   return (
-    <h1>Room</h1>
+    <Container>
+      <header>
+        <section>
+          <img src={logoImg} alt="LetMeAsk" />
+          <div> CÓDIGO DA SALA </div>
+        </section>
+      </header>
+
+      <main>
+        <div>
+          <h1>Sala React</h1>
+          <span>4 Perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar?"/>
+
+          <div>
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>       
+
+    </Container>
   );
 }
--- src/styles/button.js
+++ src/styles/button.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const ButtonStyle = styled.button`
-  margin-top: 64px;
   height: 50px;
   border-radius: 8px;
   font-weight: 500px;
